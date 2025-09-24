fe30f2017eab5355ce4e609ff85f0fb18f3e7537
winawon02 <winawon02@gmail.com>
2025-09-24 19:03:18 +0900

    .

---
+++ js/geno.js
@@ -0,0 +1,66 @@
+$(function () {
+
+
+
+	$.scrollEvent = function(){
+		var saTriggerMargin = 200;
+        var saElementList = $('.sa');
+		const scr_top = $(window).scrollTop();
+		const win_h = window.innerHeight;
+
+		const idx_01 = $('.page');
+        const land_h = $(idx_01).height();
+        const h_01 = idx_01.innerHeight();
+
+		if(land_h<scr_top+win_h){
+            $('.float_con').hide();
+        }else{
+			$('.float_con').css('display','flex');
+		}
+
+		$(saElementList).each(function(index,item){
+            if(!$(item).hasClass('show')){
+                var margin = saTriggerMargin;
+                if($(item).data('type') == 'main'){
+                    margin = 0;
+                }
+                if(window.innerHeight > item.getBoundingClientRect().top+margin){
+                    $(item).addClass('show');
+                }
+            }
+        })
+
+
+	}
+	$.scrollEvent();
+
+	// 전체 스크롤 이벤트
+    $(window).scroll(function(){
+        $.scrollEvent();
+    })
+
+	$.float_btn = function(){
+		$('.float_btn').on('click',function(){
+			const win_h = window.innerHeight;
+			const idx_01 = $('.form');
+			const land_h = $(idx_01).offset().top;
+
+			const form_move = land_h-100;
+
+			$('body, html').animate({scrollTop : form_move}, 400);
+
+		})
+	}
+
+
+	
+
+	$.initView = function () {
+		$.float_btn();
+
+	};
+	$.initView();
+
+})
+
+

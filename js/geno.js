2f59baea0b0ae55404ad099ebcadbc1af5d394e5
winawon02 <winawon02@gmail.com>
2025-09-24 22:48:26 +0900

    .

--- js/geno.js
+++ js/geno.js
@@ -63,4 +63,13 @@ $(function () {
 
 })
 
+$(window).on('load', function(){
+	$('.review_slide').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true
+	});
+});
 
